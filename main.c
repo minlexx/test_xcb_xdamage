d0024e4fb6b1a19fc2c33ee4b180c9c69d000a7c
Alexey Min <alexey.min@gmail.com>
2017-03-23 12:51:44 +0500

    Use XRandR to get primary output position and size

--- main.c
+++ main.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 #include <sys/ipc.h>
 #include <sys/shm.h>
@@ -88,6 +89,7 @@ int load_x_extensions(xcb_connection_t *conn) {
     }
     printf("Loaded XRandR extension version %d.%d\n",
            xrandr_version->major_version, xrandr_version->minor_version);
+    free(xrandr_version);
 
     return 1;
 }
@@ -107,6 +109,8 @@ int main(int argc, char *argv[])
 {
     printf("Hello World!\n");
 
+    int i = 0;
+
     int screen_preferred = 0;
     xcb_connection_t *xconn = xcb_connect(NULL, &screen_preferred);
 
@@ -124,6 +128,7 @@ int main(int argc, char *argv[])
     if (!screen) {
         fprintf(stderr, "Cannot get parameters of the default screen!\n");
         xcb_disconnect(xconn);
+        return EXIT_FAILURE;
     }
 
     printf("Preferred screen size: %d x %d, depth %d\n",
@@ -131,6 +136,82 @@ int main(int argc, char *argv[])
            (int)screen->height_in_pixels,
            (int)screen->root_depth);
     root_window = screen->root;
+
+    // get screen configurations using XRandR
+    // 1. get primary output ID
+    xcb_randr_output_t primary_output_xid = XCB_NONE;
+    xcb_randr_get_output_primary_reply_t *primary_reply = xcb_randr_get_output_primary_reply(
+        xconn, xcb_randr_get_output_primary(xconn, root_window), NULL);
+    if (primary_reply) {
+        primary_output_xid = primary_reply->output;
+        printf("RandR: got primary output XID = %ul\n", primary_output_xid);
+        free(primary_reply);
+    }
+
+    // 2. get screen configurations
+    xcb_randr_get_screen_resources_cookie_t screenres_cookie =
+            xcb_randr_get_screen_resources(xconn, root_window);
+    xcb_randr_get_screen_resources_reply_t *screenres_reply =
+            xcb_randr_get_screen_resources_reply(xconn, screenres_cookie, NULL);
+    // The returned value must be freed by the caller using free()
+    if (!screenres_reply) {
+        fprintf(stderr, "Cannot get XRandR screens configuration!\n");
+        xcb_disconnect(xconn);
+        return EXIT_FAILURE;
+    }
+    printf("XRandR screen resources:\n  num crtcs: %d\n  num outputs: %d\n  num modes: %d\n",
+           (int)screenres_reply->num_crtcs,
+           (int)screenres_reply->num_outputs,
+           (int)screenres_reply->num_modes
+    );
+
+    // 3. loop through outputs to get information about primary output
+    char primary_screen_name[32] = {0};   // primary output name
+    xcb_rectangle_t primary_screen_rect;  // primary output rectangle
+    // --------------------------------------
+    xcb_timestamp_t config_timestamp = screenres_reply->config_timestamp;
+    int num_outputs = xcb_randr_get_screen_resources_outputs_length(screenres_reply);
+    xcb_randr_output_t *randr_outputs = xcb_randr_get_screen_resources_outputs(screenres_reply);
+    for (i=0; i<num_outputs; i++) {
+        xcb_randr_get_output_info_reply_t *output_info = xcb_randr_get_output_info_reply(
+                    xconn,
+                    xcb_randr_get_output_info(xconn, randr_outputs[i], config_timestamp),
+                    NULL);
+        // checks
+        if (output_info == NULL) continue;
+        if ((output_info->crtc == XCB_NONE) ||
+                (output_info->connection == XCB_RANDR_CONNECTION_DISCONNECTED)) {
+            free(output_info);
+            continue;
+        }
+        if (primary_output_xid == randr_outputs[i]) {
+            // now get some real info about output's CRTC
+            xcb_randr_get_crtc_info_reply_t *crtc_info = xcb_randr_get_crtc_info_reply(
+                        xconn,
+                        xcb_randr_get_crtc_info(xconn, output_info->crtc, config_timestamp),
+                        NULL);
+            if (crtc_info) {
+                //
+                uint8_t *output_name = xcb_randr_get_output_info_name(output_info);
+                int namelen = xcb_randr_get_output_info_name_length(output_info);
+                strncpy(primary_screen_name, output_name, sizeof(primary_screen_name)-1);
+                primary_screen_rect.x = crtc_info->x;
+                primary_screen_rect.y = crtc_info->y;
+                primary_screen_rect.width = crtc_info->width;
+                primary_screen_rect.height = crtc_info->height;
+                //
+                printf("  Found primary output info!\n");
+                printf("    name = %s, namelen = %d\n", primary_screen_name, namelen);
+                printf("    pos (%d,%d) sz (%d,%d)\n", crtc_info->x, crtc_info->y,
+                       crtc_info->width, crtc_info->height);
+                //
+                free(crtc_info);
+            }
+        }
+        //
+        free(output_info);
+    }
+    free(screenres_reply);
 
     // in XCB we need to manually generate IDs
     xcb_damage_damage_t dmg = xcb_generate_id(xconn);
@@ -168,7 +249,6 @@ int main(int argc, char *argv[])
     printf("Event loop ended, cleanup, exit\n");
 
     xcb_damage_destroy(xconn, dmg);
-
     xcb_disconnect(xconn);
     return 0;
 }
